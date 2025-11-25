c2169b3d97b4c2154d1ea8f01bd6ddd868b86555
shnlyjsh <shnlyjsh@gmail.com>
2025-11-25 22:13:41 +0800

    ui adjustments for login and signup page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -98,14 +98,6 @@ class _LoginScreenState extends State<LoginScreen> {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(), // Removes keyboard when tapping outside input fields
       child: Scaffold(
-        // Non-existent app bar lol
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          foregroundColor: Colors.white,
-          automaticallyImplyLeading: false,
-        ),
-        extendBodyBehindAppBar: true,
         body: Stack(
           children: [
             // Gradient background
@@ -130,15 +122,18 @@ class _LoginScreenState extends State<LoginScreen> {
                       Center(
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
                           children: [
                             GradientBorderText(
                               text: 'WELCOME TO',
                               strokeWidth: 8,
                               textStyle: const TextStyle(
                                 color: Colors.white,
-                                fontSize: 18,
+                                fontSize: 48,
                                 fontFamily: 'Iceland',
-                                fontWeight: FontWeight.w600,
+                                fontWeight: FontWeight.bold,
+                                height: 0.1,
+                                letterSpacing: 0,
                               ),
                               gradient: LinearGradient(
                                 colors: [
@@ -149,8 +144,8 @@ class _LoginScreenState extends State<LoginScreen> {
                               shadows: [
                                 Shadow(
                                   blurRadius: 10.0,
-                                  color: Colors.black.withOpacity(0.5),
-                                  offset: Offset(0, 10.0),
+                                  color: Colors.black.withOpacity(0.4),
+                                  offset: Offset(0, 8.0),
                                 ),
                               ],
                             ),
@@ -160,9 +155,11 @@ class _LoginScreenState extends State<LoginScreen> {
                               strokeWidth: 8,
                               textStyle: const TextStyle(
                                 color: Colors.white,
-                                fontSize: 48,
+                                fontSize: 110,
                                 fontFamily: 'Iceland',
-                                fontWeight: FontWeight.w700,
+                                fontWeight: FontWeight.bold,
+                                height: .8,
+                                letterSpacing: -3,
                               ),
                               gradient: LinearGradient(
                                 colors: [
@@ -173,7 +170,7 @@ class _LoginScreenState extends State<LoginScreen> {
                               shadows: [
                                 Shadow(
                                   blurRadius: 10.0,
-                                  color: Colors.black.withOpacity(0.5),
+                                  color: Colors.black.withOpacity(0.7),
                                   offset: Offset(0, 10.0),
                                 ),
                               ],
@@ -181,146 +178,142 @@ class _LoginScreenState extends State<LoginScreen> {
                           ]
                         ),
                       ),
+                      const SizedBox(height: 20),
                       // Center box with login form
-                      SafeArea(
-                        child: Center(
-                          child: SingleChildScrollView(
-                            padding: const EdgeInsets.symmetric(horizontal: 24),
-                            // Container for the login form
-                            child: Card(
-                              color: Colors.white,
-                              elevation: 1,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(32),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(20),
-                                child: Column(
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        child: Card(
+                          color: Colors.white,
+                          elevation: 1,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                // Email field
+                                const SizedBox(height: 12),
+                                TextField(
+                                  controller: _emailController,
+                                  keyboardType: TextInputType.emailAddress,
+                                  style: const TextStyle(color: Colors.black),
+                                  decoration: InputDecoration(
+                                    hintText: 'Email',
+                                    hintStyle: TextStyle(color: Colors.black45),
+                                    filled: true,
+                                    fillColor: Colors.grey.withOpacity(0.1),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                      borderSide: BorderSide.none,
+                                    ),
+                                    prefixIcon: GradientIcon(
+                                      icon: Icons.mail,
+                                      size: AppDesign.sBtnIconSize,
+                                      gradient: LinearGradient(colors: [
+                                        AppDesign.primaryGradientStart,
+                                        AppDesign.primaryGradientEnd
+                                      ]),
+                                    ),
+                                  ),
+                                ),
+                                // Password field
+                                const SizedBox(height: 12),
+                                TextField(
+                                  controller: _passwordController,
+                                  style: const TextStyle(color: Colors.black),
+                                  obscureText: !_passwordVisible,
+                                  decoration: InputDecoration(
+                                    hintText: 'Password',
+                                    hintStyle: TextStyle(color: Colors.black45),
+                                    filled: true,
+                                    fillColor: Colors.grey.withOpacity(0.1),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                      borderSide: BorderSide.none,
+                                    ),
+                                    prefixIcon: GradientIcon(
+                                      icon: Icons.lock,
+                                      size: AppDesign.sBtnIconSize,
+                                      gradient: LinearGradient(colors: [
+                                        AppDesign.primaryGradientStart,
+                                        AppDesign.primaryGradientEnd
+                                      ]),
+                                    ),
+                                    suffixIcon: IconButton(
+                                      icon: GradientIcon(
+                                        icon: _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                                        size: AppDesign.sBtnIconSize,
+                                        gradient: LinearGradient(colors: [
+                                          AppDesign.primaryGradientStart,
+                                          AppDesign.primaryGradientEnd
+                                        ]),
+                                      ),
+                                      onPressed: () {
+                                        setState(() {
+                                          _passwordVisible = !_passwordVisible;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                // Sign in button
+                                const SizedBox(height: 16),
+                                SizedBox(
+                                  width: 2000.0,
+                                  height: 50.0,
+                                  child: GradientBorderButton(
+                                    onPressed: () async {
+                                      await login();
+                                    },
+                                    gradient: LinearGradient(colors: [
+                                        AppDesign.primaryGradientStart,
+                                        AppDesign.primaryGradientEnd
+                                      ]),
+                                    borderRadius: AppDesign.sBtnBorderRadius,
+                                    child: const Text(
+                                      'Log In',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                //Sign up field
+                                const SizedBox(height: 16),
+                                Row(
                                   mainAxisSize: MainAxisSize.min,
                                   children: [
-                                    // Email field
-                                    const SizedBox(height: 12),
-                                    TextField(
-                                      controller: _emailController,
-                                      keyboardType: TextInputType.emailAddress,
-                                      style: const TextStyle(color: Colors.black),
-                                      decoration: InputDecoration(
-                                        hintText: 'Email',
-                                        hintStyle: TextStyle(color: Colors.black45),
-                                        filled: true,
-                                        fillColor: Colors.grey.withOpacity(0.1),
-                                        border: OutlineInputBorder(
-                                          borderRadius: BorderRadius.circular(10),
-                                          borderSide: BorderSide.none,
-                                        ),
-                                        prefixIcon: GradientIcon(
-                                          icon: Icons.mail,
-                                          size: AppDesign.sBtnIconSize,
-                                          gradient: LinearGradient(colors: [
-                                            AppDesign.primaryGradientStart,
-                                            AppDesign.primaryGradientEnd
-                                          ]),
-                                        ),
-                                      ),
+                                    const Text(
+                                      "Don't have an account? ",
+                                      style: TextStyle(color: Colors.black45),
                                     ),
-                                    // Password field
-                                    const SizedBox(height: 12),
-                                    TextField(
-                                      controller: _passwordController,
-                                      style: const TextStyle(color: Colors.black),
-                                      obscureText: !_passwordVisible,
-                                      decoration: InputDecoration(
-                                        hintText: 'Password',
-                                        hintStyle: TextStyle(color: Colors.black45),
-                                        filled: true,
-                                        fillColor: Colors.grey.withOpacity(0.1),
-                                        border: OutlineInputBorder(
-                                          borderRadius: BorderRadius.circular(10),
-                                          borderSide: BorderSide.none,
-                                        ),
-                                        prefixIcon: GradientIcon(
-                                          icon: Icons.lock,
-                                          size: AppDesign.sBtnIconSize,
-                                          gradient: LinearGradient(colors: [
-                                            AppDesign.primaryGradientStart,
-                                            AppDesign.primaryGradientEnd
-                                          ]),
-                                        ),
-                                        suffixIcon: IconButton(
-                                          icon: GradientIcon(
-                                            icon: _passwordVisible ? Icons.visibility : Icons.visibility_off,
-                                            size: AppDesign.sBtnIconSize,
-                                            gradient: LinearGradient(colors: [
-                                              AppDesign.primaryGradientStart,
-                                              AppDesign.primaryGradientEnd
-                                            ]),
-                                          ),
-                                          onPressed: () {
-                                            setState(() {
-                                              _passwordVisible = !_passwordVisible;
-                                            });
-                                          },
-                                        ),
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(builder: (context) => const SignupScreen()),
+                                        );
+                                      },
+                                      style: TextButton.styleFrom(
+                                        padding: EdgeInsets.zero,
+                                        minimumSize: const Size(0, 0),
+                                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
                                       ),
-                                    ),
-                                    // Sign in button
-                                    const SizedBox(height: 16),
-                                    SizedBox(
-                                      width: 2000.0,
-                                      height: 50.0,
-                                      child: GradientBorderButton(
-                                        onPressed: () async {
-                                          await login();
-                                        },
-                                        gradient: LinearGradient(colors: [
-                                            AppDesign.primaryGradientStart,
-                                            AppDesign.primaryGradientEnd
-                                          ]),
-                                        borderRadius: AppDesign.sBtnBorderRadius,
-                                        child: const Text(
-                                          'Log In',
-                                          style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.bold,
-                                          ),
+                                      child: Text(
+                                        'Sign Up now',
+                                        style: TextStyle(
+                                          color: AppDesign.primaryGradientEnd,
+                                          fontWeight: FontWeight.bold,
                                         ),
-                                      ),
-                                    ),
-                                    //Sign up field
-                                    const SizedBox(height: 16),
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        const Text(
-                                          "Don't have an account? ",
-                                          style: TextStyle(color: Colors.black45),
-                                        ),
-                                        TextButton(
-                                          onPressed: () {
-                                            Navigator.push(
-                                              context,
-                                              MaterialPageRoute(builder: (context) => const SignupScreen()),
-                                            );
-                                          },
-                                          style: TextButton.styleFrom(
-                                            padding: EdgeInsets.zero,
-                                            minimumSize: const Size(0, 0),
-                                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                                          ),
-                                          child: Text(
-                                            'Sign Up now',
-                                            style: TextStyle(
-                                              color: AppDesign.primaryGradientEnd,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          )
-                                        )
-                                      ]
+                                      )
                                     )
-                                  ],
-                                ),
-                              ),
+                                  ]
+                                )
+                              ],
                             ),
                           ),
                         ),
