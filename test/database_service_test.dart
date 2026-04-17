aaff727912d9fabc1ab473e9f076d9c2d229d88e
madgallego <mharc.gallego@gmail.com>
2026-04-17 20:54:15 +0800

    ADD: Dependecy and imports

---
+++ test/database_service_test.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
